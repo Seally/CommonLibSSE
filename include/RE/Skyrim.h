56ff18718964cc0579a9e7139e4f99df8033843a
Seally <Seally@users.noreply.github.com>
2022-12-29 21:40:38 +0000

    maintenance

--- include/RE/Skyrim.h
+++ include/RE/Skyrim.h
@@ -1048,6 +1048,7 @@
 #include "RE/S/SmithingMenu.h"
 #include "RE/S/SneakHandler.h"
 #include "RE/S/SoulLevels.h"
+#include "RE/S/SoulsTrapped.h"
 #include "RE/S/SoundLevels.h"
 #include "RE/S/SpellItem.h"
 #include "RE/S/SprintHandler.h"
